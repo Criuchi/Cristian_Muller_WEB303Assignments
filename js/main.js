462092c35c7f8b8199e08497e5edbb8fd5e92f38
Cristian Hernan Muller <121682366+Criuchi@users.noreply.github.com>
2023-09-23 12:00:42 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,25 +27,29 @@ function request(){
 
 
 $(function() {
-    // Attach a click event handler to all anchor tags in the menu
+    
     $('#prospect').on('click', function() {
-  
+        request();
+    });
+});
+
+function request(){
         var xhr = new XMLHttpRequest();
         xhr.open('GET','prospect.html', true);
-  
+
         xhr.onload = function() {
             if (xhr.status === 200) {
                 // Animate the #content div to hide it
                 $('#content').fadeOut('fast', function() {
-                    // Replace the content with the loaded HTML
-                    $('#content').html(xhr.responseText);
-                    // Animate the #content div to show the new content
-                    $('#content').fadeIn('fast');
-                });
-            } 
-        };
-  
-        // Send the request
-        xhr.send();
-    });
-  });
+                // Replace the content with the loaded HTML
+                $('#content').html(xhr.responseText);
+                // Animate the #content div to show the new content
+                $('#content').fadeIn('fast');
+            });
+        } 
+    }
+
+    // Send the request
+    xhr.send();
+
+};
