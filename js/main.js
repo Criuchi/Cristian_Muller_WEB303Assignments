c7a4939b4f0eeee585f08c6609180dd23734bddf
Cristian Hernan Muller <121682366+Criuchi@users.noreply.github.com>
2023-09-23 11:48:42 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,11 @@
 
 
 /*  $(function(){
-    //Creating the method to replace the value of #amount
     $("#prospect").on('click', function(){
         request();
     });
 
-});
+/* });
 
 function request(){
  var xhr = new XMLHttpRequest;
@@ -24,51 +23,29 @@ function request(){
   }
  }   
   xhr.send();
-}  */
+}  */ 
 
 
-    $(function(){
-    var content = document.getElementById('content');
-
-    function request() {
+$(function() {
+    // Attach a click event handler to all anchor tags in the menu
+    $('#prospect').on('click', function() {
+  
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-
+        xhr.open('GET','prospect.html', true);
+  
         xhr.onload = function() {
             if (xhr.status === 200) {
-                animateContent(xhr.responseText);
+                // Animate the #content div to hide it
+                $('#content').fadeOut('fast', function() {
+                    // Replace the content with the loaded HTML
+                    $('#content').html(xhr.responseText);
+                    // Animate the #content div to show the new content
+                    $('#content').fadeIn('fast');
+                });
             } 
         };
-
+  
+        // Send the request
         xhr.send();
-    }
-
-    function animateContent(newContent) {
-        $(content).fadeOut(500, function () {
-            $(this).html(newContent).fadeIn(500);
-        });
-    }
-
-    // Handle clicks on links
-    var links = document.querySelectorAll('a');
-    links.forEach(function (link) {
-        link.on('click', function (e) {
-            e.preventDefault();
-
-            var contentFile;
-
-            if (this.id === 'prospect') {
-                contentFile = 'prospect.html';
-            } else if (this.id === 'convert') {
-                contentFile = 'convert.html';
-            } else if (this.id === 'retain') {
-                contentFile = 'retain.html';
-            }
-
-            $(content).fadeOut(500, function () {
-                $(this).html('');
-                request(contentFile);
-            });
-        });
     });
-});  
+  });
